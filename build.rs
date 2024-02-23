ca9567af64739a06ea504bcf6542f265b8c2d1e5
lake <102535481@qq.com>
2024-02-23 10:54:59 +0800

    init

--- build.rs
+++ build.rs
@@ -1,7 +1,28 @@
 extern crate cc;
-
+extern crate cmake;
+fn env<N: AsRef<str>>(name: N) -> String {
+    option_env(name).expect("missing env var")
+}
+fn option_env<N: AsRef<str>>(name: N) -> Option<String> {
+    let name = name.as_ref();
+    eprintln!("cargo:rerun-if-env-changed={}", name);
+    std::env::var(name).ok()
+}
 fn main() {
     let mut builder: cc::Build = cc::Build::new();
+
+    let zsign_files = [
+        "zsign/common/base64.cpp",
+        "zsign/common/json.cpp",
+        "zsign/common/common.cpp",
+        "zsign/archo.cpp",
+        "zsign/bundle.cpp",
+        "zsign/macho.cpp",
+        "zsign/openssl.cpp",
+        "zsign/signing.cpp",
+        "zsign/zsign.cpp",
+    ];
+
     //依赖openssl
     builder
         .cpp(true)
@@ -9,23 +30,12 @@ fn main() {
         .std("c++11")
         .flag("-c")
         .shared_flag(false)
-        .flag("-I/opt/homebrew/Cellar/openssl@3/3.2.0_1/include")
-        .flag("-L/opt/homebrew/Cellar/openssl@3/3.2.0_1/lib")
-        .file("zsign/common/base64.cpp")
-        .file("zsign/common/json.cpp")
-        .file("zsign/common/common.cpp")
-        .file("zsign/archo.cpp")
-        .file("zsign/bundle.cpp")
-        .file("zsign/macho.cpp")
-        .file("zsign/openssl.cpp")
-        .file("zsign/signing.cpp")
-        .file("zsign/zsign.cpp")
-        .file("zsign/zsign.cpp")
+        .files(zsign_files.iter())
+        .include(env("DEP_OPENSSL_INCLUDE")) //openssl 头文件
         .compile("zsign");
 
     println!("cargo:rustc-link-lib=ssl");
     println!("cargo:rustc-link-lib=crypto");
-    println!("cargo:rustc-link-search=/opt/homebrew/Cellar/openssl@3/3.2.0_1/lib");
     println!("cargo:rerun-if-changed=zsign/common/common.cpp");
     println!("cargo:rerun-if-changed=build.rs");
 }
